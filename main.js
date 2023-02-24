5db486fbbcd16b80fb9da3805fafd624db7d08bb
Alex_K <as.kirsanov@yandex.ru>
2023-02-25 02:38:48 +0300

    done

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
 import { sendInTlg } from './functions.js';
 
 function print_CV() {
-    let img = document.createElement('img');
-    img.src = "./Print_CV.png";
-    img.width = 0;
-    document.body.append(img);
-    const win = window.open();
-    win.document.write('<img src="./Print_CV.png" />');
-    setTimeout(() => {
-        win.print();
-        img.remove();
-        win.close();
-    },10);
+    window.print();
 }
         
 const sendMessage = (e) => {
